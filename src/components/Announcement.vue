ea72267ddd48dd78d89606f99bff3c8d784b3e8e
Gytis <101097792+LeoGytis@users.noreply.github.com>
2023-10-25 17:48:27 +0300

    overflow

--- src/components/Announcement.vue
+++ src/components/Announcement.vue
@@ -1,24 +1,22 @@
 <template>
   <div class="font-medium rounded-lg border border-gray-300">
-    <div class="p-4">
-      <div class="flex justify-between pb-4">
-        <h1 class="font-semibold">Announcement</h1>
-        <div class="flex">
-          <button @click="fetchProducts" class="text-xs rounded-lg text-gray-400 border border-gray-200 py-1 px-3 mr-3">
-            Refresh
-          </button>
-          <div class="flex items-center gap-4 text-xs rounded-lg text-gray-400 border border-gray-200 py-1 px-3">
-            <span>Sort by:</span>
-            <select v-model="sortOption" @change="sortProducts" class="bg-transparent">
-              <option value="title">Title</option>
-              <option value="rating">Rating</option>
-              <option value="price">Price</option>
-            </select>
-          </div>
+    <div class="flex justify-between p-4">
+      <h1 class="font-semibold">Announcement</h1>
+      <div class="flex">
+        <button @click="fetchProducts" class="text-xs rounded-lg text-gray-400 border border-gray-200 py-1 px-3 mr-3">
+          Refresh
+        </button>
+        <div class="flex items-center gap-4 text-xs rounded-lg text-gray-400 border border-gray-200 py-1 px-3">
+          <span>Sort by:</span>
+          <select v-model="sortOption" @change="sortProducts" class="bg-transparent">
+            <option value="title">Title</option>
+            <option value="rating">Rating</option>
+            <option value="price">Price</option>
+          </select>
         </div>
       </div>
     </div>
-    <div class="flex flex-col justify-center items-center">
+    <div class="justify-center items-center h-52 overflow-hidden">
       <p v-if="isLoading">
       <div class="flex items-center p-6">
         <v-icon name="fa-spinner" animation="spin-pulse" class="mr-3" />Loading...
@@ -28,12 +26,12 @@
       <ul v-else>
         <li v-for="product in products" :key="product.id">
           <div class="flex flex-col rounded-lg border border-gray-200 bg-gray-50 py-2 px-4 m-4">
-            <div class="flex justify-between mb-10">
+            <div class="flex justify-between mb-2">
               <h2>{{ product.title }}</h2>
               <p>Rating: {{ product.rating }}</p>
               <p>Price: ${{ product.price }}</p>
             </div>
-            <p>{{ product.description }}</p>
+            <p class="text-xs">{{ product.description }}</p>
           </div>
         </li>
       </ul>
